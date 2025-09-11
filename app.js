ecd738e301bdee63083238d39c114980435724eb
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-09-11 19:16:47 -0300

    Import modules and messages, set views and use index router

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("node:path");
+const messages = require("./messages");
+const indexRouter = require("./routes/index");
+
 const app = express();
 const PORT = process.env.PORT;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.use("/", indexRouter);
 
 app.listen(PORT, (error) => {
   if (error) {
