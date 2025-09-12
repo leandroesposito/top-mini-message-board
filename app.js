268c27b22603235a58c4c093adbec08ac67e245e
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-09-12 16:54:16 -0300

    Use new router and urlencoder

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("node:path");
 const messages = require("./messages");
 const indexRouter = require("./routes/index");
+const newRouter = require("./routes/new");
 
 const app = express();
 const PORT = process.env.PORT;
@@ -13,7 +14,10 @@ app.set("view engine", "ejs");
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
+app.use(express.urlencoded());
+
 app.use("/", indexRouter);
+app.use("/new", newRouter);
 
 app.listen(PORT, (error) => {
   if (error) {
