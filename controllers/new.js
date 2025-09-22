145b5fb3bf2665b28ab453c930b91c9ea0e926a5
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-09-21 21:09:38 -0300

    Add length validations

--- controllers/new.js
+++ controllers/new.js
@@ -1,10 +1,30 @@
 const db = require("../db/queries");
+const { body, validationResult } = require("express-validator");
+
+const validators = [
+  body("username")
+    .trim()
+    .isLength({ min: 1, max: 255 })
+    .withMessage("username must be at least 1 character and at most 255"),
+  body("text")
+    .trim()
+    .isLength({ min: 1, max: 255 })
+    .withMessage("text must be at least 1 character and at most 255"),
+];
 
 async function newGet(req, res) {
   res.render("new", { title: "New Message" });
 }
 
 async function newPost(req, res) {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .render("new", { title: "New Message", errors: errors.array() });
+  }
+
   const { username, text } = req.body;
   await db.postMessage(username, text);
   res.redirect("/");
@@ -12,5 +32,5 @@ async function newPost(req, res) {
 
 module.exports = {
   newGet,
-  newPost,
+  newPost: [validators, newPost],
 };
